42207805e6f6dc9f410a08890319a866a16c0852
Milan <comiv92@gmail.com>
2020-03-24 18:22:39 +0100

    Fixes the button props

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,11 +26,11 @@ const controls = [
         <button
           className={classes.ControlButton}
           disabled={!props.purchasable}
-          onClick={props.ordered}>RESET</button>
+          onClick={props.reset}>RESET</button>
         <button
           className={classes.ControlButton}
           disabled={!props.purchasable}
-          onClick={props.reset}>ORDER NOW</button>
+          onClick={props.ordered}>ORDER NOW</button>
       </div>
     </div>
  );
