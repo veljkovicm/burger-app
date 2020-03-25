2d3a7f5b1e2ee4fccc6dc885ad75c1c51a3d1773
Milan <comiv92@gmail.com>
2020-03-25 17:22:07 +0100

    adds logo and mobile navigation

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './DrawerToggle.module.css';
+
+const drawerToggle = (props) => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>MENU</div>
+)
+
+export default drawerToggle;
