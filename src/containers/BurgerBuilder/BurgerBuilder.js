f5975cb16558e3fb91876c56ab8ec04733852ac4
Milan <comiv92@gmail.com>
2020-03-26 17:02:59 +0100

    adds axios and order posting

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -89,7 +90,25 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    //alert('You continue!');
+    const order = {
+      igredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Milan Veljkovic',
+        address: {
+          street: 'Test St 1',
+          zipCode: '23512',
+          country: 'Serbia',
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'premium'
+    }
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+      ;
   }
 
   render() {
