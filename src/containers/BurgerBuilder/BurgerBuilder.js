9d6b6d60ab733c4d022f9dba3bd1568e9fd84ce1
Milan <comiv92@gmail.com>
2020-03-26 19:00:56 +0100

    fixes the api link and the error message condition

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,7 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://burger-app-vm.firebaseio.com/ingredients')
+    axios.get('https://burger-app-vm.firebaseio.com/ingredients.json')
       .then(response => {
         this.setState({ingredients: response.data});
       })
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -34,7 +34,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
         <Modal 
           show={this.state.error}
           modalClosed={this.errorConfirmedHandler}>
-          {this.state.error ? this.state.error : null}
+          {this.state.error ? this.state.error.message : null}
         </Modal>
         <WrappedComponent {...this.props} />
       </Aux>
