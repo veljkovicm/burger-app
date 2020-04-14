143284c190ad0c2cff4189891c2d9c42771d3c18
Milan <comiv92@gmail.com>
2020-04-14 15:17:49 +0200

    adds the logout function

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
@@ -22,8 +23,11 @@ class  Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar  drawerToggleClicked={this.sideDrawerToggleHandler} />
-        <SideDrawer  
+        <Toolbar  
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer} 
           closed={this.sideDrawerClosedHandler}/>
         <main className={classes.Content}>
@@ -34,5 +38,11 @@ class  Layout extends Component {
   }
 };
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
 
-export default Layout;
+
+export default connect(mapStateToProps)(Layout);
