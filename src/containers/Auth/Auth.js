cf819db8f8fb4ab3d620d31b80747bc11ad60a9e
Milan <comiv92@gmail.com>
2020-04-14 14:23:56 +0200

    adds auth error message

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -122,8 +122,18 @@ class Auth extends Component {
     if(this.props.loading) {
       form = <Spinner />
     }
+
+    let errorMessage = null;
+
+    if(this.props.error) {
+      errorMessage = (
+        <p>{this.props.error.message}</p>
+      )
+    }
+
     return (
       <div className={classes.Auth}>
+        {errorMessage}
         <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
@@ -139,7 +149,8 @@ class Auth extends Component {
 
 const mapStateToProps = state => {
   return {
-    loading: state.auth.loading
+    loading: state.auth.loading,
+    error: state.auth.error
   };
 };
 
