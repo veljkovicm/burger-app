7636ecb05e27aeadced91ec1644ac03c453c7ab5
Milan <comiv92@gmail.com>
2020-04-14 14:16:33 +0200

    adds user signup/login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -6,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData
+    idToken: token,
+    userId: userId,
   };
 };
 
@@ -20,8 +23,27 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC4bTC7mufMX5TeDcW9ApapSIb19Kbr2hM';
+    if(!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC4bTC7mufMX5TeDcW9ApapSIb19Kbr2hM';
+    }
+    axios.post(url,authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      })
   }
 }
