a9c73dd70e179681d8b238ad393ec471e9147000
Milan <comiv92@gmail.com>
2020-05-01 11:41:10 +0200

    adds hooks to layout component

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Aux from '../../hoc/Auxiliary';
@@ -6,36 +6,31 @@ import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class  Layout extends Component {
-  state = {
-    showSideDrawer: false
-  }
+const Layout = props => {
+  const [ sideDrawerVisible, setSideDrawerVisible ] = useState(false);
   
-  sideDrawerClosedHandler = () => {
-    this.setState({showSideDrawer: false})
+  const sideDrawerClosedHandler = () => {
+    setSideDrawerVisible(false);
   }
-  sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
-      return {showSideDrawer: !prevState.showSideDrawer}
-    });
+  const sideDrawerToggleHandler = () => {
+    setSideDrawerVisible(!sideDrawerVisible);
   }
 
-  render() {
-    return (
-      <Aux>
-        <Toolbar  
-          isAuth={this.props.isAuthenticated}
-          drawerToggleClicked={this.sideDrawerToggleHandler} />
-        <SideDrawer
-          isAuth={this.props.isAuthenticated}
-          open={this.state.showSideDrawer} 
-          closed={this.sideDrawerClosedHandler}/>
-        <main className={classes.Content}>
-          {this.props.children}
-       </main>
-     </Aux>
-    )
-  }
+  return (
+    <Aux>
+      <Toolbar  
+        isAuth={props.isAuthenticated}
+        drawerToggleClicked={sideDrawerToggleHandler} />
+      <SideDrawer
+        isAuth={props.isAuthenticated}
+        open={sideDrawerVisible} 
+        closed={sideDrawerClosedHandler}/>
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </Aux>
+  )
+  
 };
 
 const mapStateToProps = state => {
